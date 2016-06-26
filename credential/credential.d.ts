6e2e003caf909b17f5dc4e9f7af6f1536b097973
Julien Zakaib <julien.zakaib@gmail.com>
2016-06-26 04:05:04 -0400

    Updated declaration file for Credential npm module according to version 2.0.0 - commit #91df2fa of ericelliot/credential/credential.js

--- credential/credential.d.ts
+++ credential/credential.d.ts
@@ -1,18 +1,61 @@
-// Type definitions for credential
-// Project: https://github.com/ericelliott/credential
-// Definitions by: Phú <https://github.com/phuvo>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
 declare module 'credential' {
 
-	type HashCallback = (err: Error, hash: string) => void;
-	type VerifyCallback = (err: Error, isValid: boolean) => void;
+    type HashCallback = (err: Error, hashJSON: HashJSON) => void;
+    type VerifyCallback = (err: Error, isValid: boolean) => void;
+    
+    type Options = {
+        keyLength: number,
+        work: number
+    };
+    
+    type HashJSON = {
+        hash: string,
+        salt: string,
+        keyLength: number,
+        hashMethod: string,
+        iterations: number
+    };
+    
+    interface Credential {
+        /**
+         * @callback verifyCallback
+         * @param {Error} err
+         * @param {boolean} isValid
+         */
+        
+        /**
+         * Takes a stored hash, password input from the user, and a callback, and determines whether or not the user's input matches the stored password. 
+         * @param {string} hash - A stored password hash
+         * @param {string} input - User's password input
+         * @param {verifyCallback} callback - If callback is not provided, verify returns a Promise
+         */
+        verify(hash: string, input: string, callback?: VerifyCallback): void | Promise<boolean>;
 
-	namespace credential {
-		function hash(password: string, callback: HashCallback): void;
-		function verify(hash: string, password: string, callback: VerifyCallback): void;
-	}
+        /**
+         * @callback hashCallback
+         * @param {Error} err
+         * @param {Object} hashJSON
+         * @param {string} hashJSON.hash
+         * @param {string} hashJSON.salt
+         * @param {number} hashJSON.keyLength - Bytes in hash
+         * @param {string} hashJSON.hashMethod
+         * @param {number} hashJSON.iterations
+         */
 
-	export = credential;
-
+        /**
+         * Takes a new password and creates a unique hash. Passes a JSON encoded object to the callback.
+         * @param {string} password
+         * @param {hashCallback} callback
+         */
+        hash(password: string, callback?: HashCallback): void | Promise<HashJSON>;
+        
+        /**
+         * Takes a stored hash, password input from the user, and a callback, and determines whether or not the user's input matches the stored password.
+         * @param hash - A stored password hash
+         * @param days - Days before expiry
+         */
+        expired(hash: string, days?: number): boolean;
+    }
+    
+    export function credential(options?: Options): Credential;
 }
